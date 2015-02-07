017265f0a01b2c7ff44805bd8726cddc53672126
s <stanisvs@gmail.com>
2015-02-08 00:54:49 +0300

    update format of name

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,6 @@
 class Word < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true, format:
-                     {with: /\A[a-zA-Z]+\z/, message: "only allows letters"}
+                     {with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and white spaces"}
   validates :definition, presence: true, format:
-                           {with: /\A[a-zA-Z.,!\-\s]+\z/, message: "only allows letters and white spaces"}
+                           {with: /\A[a-zA-Z.,!\-\s]+\z/, message: "only allows letters,spaces and punctuation marks"}
 end
