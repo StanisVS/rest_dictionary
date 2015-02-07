6d657f1d6a3efd78b9556c2a1d292f5eea0edd84
s <stanisvs@gmail.com>
2015-02-08 01:59:00 +0300

    update formats

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,7 @@
 class Word < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true, format:
-                     {with: /\A([a-z\s]|\([a-z\s]+\))+\z/,
+                     {with: /\A([a-z\s\-]|\([a-z\s\-]+\))+\z/,
                       message: "only allows text in low-letters and in scopes"}
   validates :definition, presence: true, format:
-                           {with: /\A([a-zA-Z.,!\-\s]|\([a-zA-Z.,!\-\s]+\))+\z/, message: "only allows letters,spaces and punctuation marks"}
+                           {with: /\A([a-zA-Z.,!;\-\s]|\([a-zA-Z.,!;\-\s]+\))+\z/, message: "only allows letters,spaces and punctuation marks"}
 end
